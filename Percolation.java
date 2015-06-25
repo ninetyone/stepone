6400d6a8a1e5ec8ef868f0fa62fb220500180006
ninetyone <akshaygoyal@Akshays-MacBook-Air.local>
2015-06-25 11:52:27 +0530

    Percolation.java

---
+++ Percolation.java
@@ -0,0 +1,86 @@
+/*------------------------------------------------------
+ *  Author:        Akshay Goyal
+ *  Written:       5/3/2015
+ *  Last updated:  8/7/2006
+ *
+ *  Compilation:   javac HelloWorld.java
+ *  Execution:     java HelloWorld
+ *  
+ *  Prints "Hello, World". By tradition, this is everyone's
+ *  first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ * 
+ */
+
+public class Percolation {
+	
+	private boolean[][] opened;
+	private int top = 0;
+	private int bottom;
+	private int size;
+	private WeightedQuickUnionUF uf;
+	
+	public Percolation(int N) {              
+		// create N-by-N grid, with all sites blocked
+		if (N < 1) 
+			throw new IndexOutOfBoundsException("Grid size " + N + " is out of bounds");
+		size = N;
+		bottom = size * size + 1;
+		uf = new WeightedQuickUnionUF(size * size + 2);
+		opened = new boolean[size][size];
+		
+	}
+	   public void open(int i, int j) {         
+		   // open site (row i, column j) if it is not open already
+		   if (i <= 0 || i > size) throw new IndexOutOfBoundsException("row index " + i + " is out of bounds");
+		   if (i <= 0 || i > size) throw new IndexOutOfBoundsException("column index " + j + " is out of bounds");
+		   
+		   if (i == 1) {
+			   uf.union(getIndex(i, j), top);
+		   }
+		   if (i == size) {
+			   uf.union(getIndex(i, j), bottom);
+		   }
+		   if (j > 1 && isOpen(i, j-1)) {
+			   uf.union(getIndex(i, j), getIndex(i, j - 1));
+		   }
+		   if (j < size && isOpen(i, j+1)) {
+			   uf.union(getIndex(i, j), getIndex(i, j + 1));
+		   }
+		   if (i > 1 && isOpen(i - 1, j)) {
+			   uf.union(getIndex(i, j), getIndex(i - 1, j));
+		   }
+		   if (i < size && isOpen(i + 1, j)) {
+			   uf.union(getIndex(i, j), getIndex(i + 1, j));
+		   }
+	   }
+	   
+	   public boolean isOpen(int i, int j) {     
+		   // is site (row i, column j) open?
+		   return opened[i - 1][j - 1];
+	   }
+	   
+	   public boolean isFull(int i, int j) {
+	   // is site (row i, column j) full?
+		   if (i > 0 && i <= size && j > 0 && j <= size) {
+			   return uf.connected(getIndex(i, j), top);
+		   }else {
+			   throw new IndexOutOfBoundsException();
+		   }
+	   }
+	   public boolean percolates() {            
+		   // does the system percolate?
+	   return uf.connected(top, bottom);
+	   }
+	   
+	   private int getIndex(int i, int j) {
+	        return size * (i - 1) + j;
+	    }
+
+	   public static void main(String[] args) {   // test client (optional)
+		   
+	   }
+
+}
